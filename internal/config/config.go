0c8776d6b76b3008eb17692d9186b9821b8c55cd
Niko Raes <niko.raes@arcadis.com>
2025-09-21 21:39:02 +0200

    fix: reorder fields in DatabaseConfig and StripeConfig for consistency

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,12 +19,12 @@ type ServerConfig struct {
 }
 
 type DatabaseConfig struct {
-	Host      string `mapstructure:"host"`
-	Port      int    `mapstructure:"port"`
-	User      string `mapstructure:"user"`
-	Password  string `mapstructure:"password"`
-	DBName    string `mapstructure:"dbname"`
-	SSLMode   string `mapstructure:"sslmode"`
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	DBName   string `mapstructure:"dbname"`
+	SSLMode  string `mapstructure:"sslmode"`
 }
 
 type AuthConfig struct {
@@ -33,10 +33,10 @@ type AuthConfig struct {
 }
 
 type StripeConfig struct {
-	SecretKey           string            `mapstructure:"secret_key"`
-	PublishableKey      string            `mapstructure:"publishable_key"`
-	WebhookSecret       string            `mapstructure:"webhook_secret"`
-	Products            []StripeProduct   `mapstructure:"products"`
+	SecretKey      string          `mapstructure:"secret_key"`
+	PublishableKey string          `mapstructure:"publishable_key"`
+	WebhookSecret  string          `mapstructure:"webhook_secret"`
+	Products       []StripeProduct `mapstructure:"products"`
 }
 
 type StripeProduct struct {
@@ -46,18 +46,32 @@ type StripeProduct struct {
 }
 
 func LoadConfig(path string) (config Config, err error) {
-		viper.AddConfigPath(path)
-		viper.SetConfigName("config")
-		viper.SetConfigType("yaml")
-		viper.SetEnvPrefix("KTRLPLANE")
-		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-       	viper.AutomaticEnv()
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.SetEnvPrefix("KTRLPLANE")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 
-		err = viper.ReadInConfig()
-		if err != nil {
-			return Config{}, fmt.Errorf("fatal error config file: %w", err)
-		}
+	// Explicitly bind env vars to config keys
+	viper.BindEnv("server.port")
+	viper.BindEnv("database.host")
+	viper.BindEnv("database.port")
+	viper.BindEnv("database.user")
+	viper.BindEnv("database.password")
+	viper.BindEnv("database.dbname")
+	viper.BindEnv("database.sslmode")
+	viper.BindEnv("auth.issuer")
+	viper.BindEnv("auth.audience")
+	viper.BindEnv("stripe.secret_key")
+	viper.BindEnv("stripe.publishable_key")
+	viper.BindEnv("stripe.webhook_secret")
 
-		err = viper.Unmarshal(&config)
-		return
+	err = viper.ReadInConfig()
+	if err != nil {
+		return Config{}, fmt.Errorf("fatal error config file: %w", err)
+	}
+
+	err = viper.Unmarshal(&config)
+	return
 }
