37f55860bbd45301b741cf79f0c54364ff3e4b79
Niko Raes <niko.raes@gmail.com>
2025-12-20 12:36:10 +0100

    feat: Add CI workflow for backend and frontend, and implement Go application configuration.

--- internal/config/config.go
+++ internal/config/config.go
@@ -108,7 +108,9 @@ func LoadConfig(path string) (config Config, err error) {
 
 	err = viper.ReadInConfig()
 	if err != nil {
-		return Config{}, fmt.Errorf("fatal error config file: %w", err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return Config{}, fmt.Errorf("fatal error config file: %w", err)
+		}
 	}
 
 	err = viper.Unmarshal(&config)
